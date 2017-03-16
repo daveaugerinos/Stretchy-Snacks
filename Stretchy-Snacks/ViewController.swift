1c552301ffe47a99f01bd6d7f56678a7005f6170
daveaugerinos <dave.augerinos@gmail.com>
2017-03-16 16:54:16 -0700

    Initial setup

---
+++ Stretchy-Snacks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stretchy-Snacks
+//
+//  Created by Dave Augerinos on 2017-03-16.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
