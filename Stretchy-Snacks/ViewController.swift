11a39562b8555ae1ca0097d2ac3884dd7e73f1bf
daveaugerinos <dave.augerinos@gmail.com>
2017-03-16 20:27:22 -0700

    Nav bar expansion and retraction with button press

--- Stretchy-Snacks/ViewController.swift
+++ Stretchy-Snacks/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var navBarView: UIView!
     @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var navBarHeightConstraint: NSLayoutConstraint!
+    private var isExpanded = false
     
     override func viewDidLoad() {
         
@@ -26,17 +28,34 @@ class ViewController: UIViewController {
         let addButton = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
         addButton.setTitle("＋", for: .normal)
         addButton.setTitleColor(.blue, for: .normal)
+        addButton.setTitleColor(.darkGray, for: .highlighted)
         addButton.addTarget(self, action: #selector(addButtonTapped(sender:)), for: .touchUpInside)
         addButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(addButton)
         
         let margins = navBarView.layoutMarginsGuide
-        addButton.centerYAnchor.constraint(equalTo: margins.centerYAnchor).isActive = true
+        addButton.topAnchor.constraint(equalTo: margins.topAnchor, constant: 20).isActive = true
         addButton.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: -3).isActive = true
     }
     
     func addButtonTapped(sender: UIButton) {
         
+        if(isExpanded) {
         
+            navBarHeightConstraint.constant = 66
+            isExpanded = false
+        }
+        
+        else {
+            
+            navBarHeightConstraint.constant = 220
+            isExpanded = true
+        }
+        
+        UIView.animate(withDuration: 0.5, delay: 0.25, options: .curveEaseIn, animations: { () -> Void in
+            
+            self.view.layoutIfNeeded()
+            
+        }, completion: nil)
     }
 }
